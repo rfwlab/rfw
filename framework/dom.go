4b95f351b1ecd0c13011f01e6288cab3e576d932
mirkobrombin <send@mirko.pm>
2024-09-17 19:36:38 +0200

    chore: update only parts that changes in the DOM instead of the whole

--- framework/dom.go
+++ framework/dom.go
@@ -1,4 +1,3 @@
-// Path: /home/mirko/Projects/personal/rfw/framework/dom.go
 //go:build js && wasm
 
 package framework
@@ -6,6 +5,7 @@ package framework
 import (
 	"fmt"
 	"regexp"
+	"strings"
 	"syscall/js"
 )
 
@@ -26,28 +26,52 @@ func UpdateDOM(componentID string, html string) {
 }
 
 func bindStoreInputs(element js.Value) {
-	inputs := element.Call("querySelectorAll", "input")
+	inputs := element.Call("querySelectorAll", "input, select, textarea")
 	for i := 0; i < inputs.Length(); i++ {
 		input := inputs.Index(i)
-		storeMatch := regexp.MustCompile(`@store:(\w+)\.(\w+)(:w)?`).FindStringSubmatch(input.Get("value").String())
+		value := input.Get("value").String()
+		storeMatch := regexp.MustCompile(`@store:(\w+)\.(\w+):w`).FindStringSubmatch(value)
 
-		if len(storeMatch) >= 3 {
+		if len(storeMatch) == 3 {
 			storeName := storeMatch[1]
 			key := storeMatch[2]
-			isWriteable := len(storeMatch) == 4 && storeMatch[3] == ":w"
 
 			store := GlobalStoreManager.GetStore(storeName)
 			if store != nil {
-				input.Set("value", store.Get(key))
-
-				if isWriteable {
-					input.Call("addEventListener", "input", js.FuncOf(func(this js.Value, args []js.Value) interface{} {
-						newValue := this.Get("value").String()
-						store.Set(key, newValue)
-						return nil
-					}))
+				storeValue := store.Get(key)
+				if storeValue == nil {
+					storeValue = ""
 				}
+				input.Set("value", fmt.Sprintf("%v", storeValue))
+
+				input.Call("addEventListener", "input", js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+					newValue := this.Get("value").String()
+					store.Set(key, newValue)
+					return nil
+				}))
 			}
 		}
 	}
+}
+
+func getConditionDependencies(condition string) ([]ConditionDependency, error) {
+	conditionParts := strings.Split(condition, "==")
+	if len(conditionParts) != 2 {
+		return nil, fmt.Errorf("Invalid condition format")
+	}
+
+	leftSide := strings.TrimSpace(conditionParts[0])
+	leftSide = strings.Replace(leftSide, "@if:", "", 1)
+
+	dependencies := []ConditionDependency{}
+
+	if strings.HasPrefix(leftSide, "store:") {
+		storeParts := strings.Split(strings.TrimPrefix(leftSide, "store:"), ".")
+		if len(storeParts) == 2 {
+			storeName, key := storeParts[0], storeParts[1]
+			dependencies = append(dependencies, ConditionDependency{storeName, key})
+		}
+	}
+
+	return dependencies, nil
 }
--- framework/html_component.go
+++ framework/html_component.go
@@ -10,27 +10,47 @@ import (
 	"regexp"
 	"sort"
 	"strings"
+	"syscall/js"
 )
 
 type HTMLComponent struct {
-	ID           string
-	Name         string
-	Template     string
-	TemplateFS   []byte
-	Dependencies map[string]Component
-	unsubscribes []func()
-	Store        *Store
-	Props        map[string]interface{}
+	ID                string
+	Name              string
+	Template          string
+	TemplateFS        []byte
+	Dependencies      map[string]Component
+	unsubscribes      []func()
+	Store             *Store
+	Props             map[string]interface{}
+	conditionContents map[string]ConditionContent
+}
+
+type ConditionContent struct {
+	conditionStr string
+	ifContent    string
+	elseContent  string
+}
+
+type ConditionalBlock struct {
+	Condition   string
+	IfContent   string
+	ElseContent string
+}
+
+type ConditionDependency struct {
+	storeName string
+	key       string
 }
 
 func NewHTMLComponent(name string, templateFs []byte, props map[string]interface{}) *HTMLComponent {
 	id := generateComponentID(name, props)
 	return &HTMLComponent{
-		ID:           id,
-		Name:         name,
-		TemplateFS:   templateFs,
-		Dependencies: make(map[string]Component),
-		Props:        props,
+		ID:                id,
+		Name:              name,
+		TemplateFS:        templateFs,
+		Dependencies:      make(map[string]Component),
+		Props:             props,
+		conditionContents: make(map[string]ConditionContent),
 	}
 }
 
@@ -64,6 +84,7 @@ func (c *HTMLComponent) Render() string {
 		placeholder := fmt.Sprintf("{{%s}}", key)
 		renderedTemplate = strings.ReplaceAll(renderedTemplate, placeholder, fmt.Sprintf("%v", value))
 	}
+
 	// Handle @store:storeName.varName syntax:
 	// - :w stands for writeable inputs
 	// - :r stands for read-only inputs (default, not required, actually not even implemented)
@@ -109,14 +130,15 @@ func replaceStorePlaceholders(template string, c *HTMLComponent) string {
 			}
 
 			unsubscribe := store.OnChange(key, func(newValue interface{}) {
-				UpdateDOM(c.ID, c.Render())
+				UpdateStoreBindings(c, storeName, key, newValue)
 			})
 			c.unsubscribes = append(c.unsubscribes, unsubscribe)
 
 			if isWriteable {
-				return fmt.Sprintf("@store:%s.%s:w", storeName, key)
+				return match
+			} else {
+				return fmt.Sprintf(`<span data-store="%s.%s">%v</span>`, storeName, key, value)
 			}
-			return fmt.Sprintf("%v", value)
 		}
 		return match
 	})
@@ -201,62 +223,134 @@ func serializeProps(props map[string]interface{}) string {
 	return sb.String()
 }
 
-type ConditionalBlock struct {
-	Condition   string
-	IfContent   string
-	ElseContent string
-}
-
 func replaceConditionals(template string, c *HTMLComponent) string {
 	ifRegex := regexp.MustCompile(`(@if:.+)([\S\s]+)(@else)([\S\s]+)(@endif)`)
-	parts := ifRegex.FindStringSubmatch(template)
-	if parts == nil || len(parts) == 0 || len(parts) < 3 {
-		return template
-	}
+	template = ifRegex.ReplaceAllStringFunc(template, func(match string) string {
+		parts := ifRegex.FindStringSubmatch(match)
+		if len(parts) < 4 {
+			return match
+		}
 
-	conditionalBlock := ConditionalBlock{}
+		conditionStr := strings.TrimSpace(parts[1])
+		ifContent := parts[2]
+		elseContent := parts[4]
 
-	conditionalBlock.Condition = strings.TrimSpace(parts[1])
-	conditionalBlock.IfContent = strings.TrimSpace(parts[2])
+		conditionID := fmt.Sprintf("cond-%x", sha1.Sum([]byte(conditionStr)))
 
-	trimmedElseContent := strings.TrimSpace(parts[4])
-	if trimmedElseContent != "@endif" {
-		conditionalBlock.ElseContent = trimmedElseContent
-	}
+		result, dependencies := evaluateCondition(conditionStr, c)
 
-	if evaluateCondition(conditionalBlock.Condition, c) {
-		template = strings.Replace(template, parts[0], conditionalBlock.IfContent, 1)
-	} else if conditionalBlock.ElseContent != "" {
-		template = strings.Replace(template, parts[0], conditionalBlock.ElseContent, 1)
-	} else {
-		template = strings.Replace(template, parts[0], "", 1)
-	}
+		c.conditionContents[conditionID] = ConditionContent{
+			conditionStr: conditionStr,
+			ifContent:    ifContent,
+			elseContent:  elseContent,
+		}
+
+		for _, dep := range dependencies {
+			storeName, key := dep.storeName, dep.key
+			store := GlobalStoreManager.GetStore(storeName)
+			if store != nil {
+				unsubscribe := store.OnChange(key, func(newValue interface{}) {
+					UpdateConditionBindings(c, conditionID, conditionStr)
+				})
+				c.unsubscribes = append(c.unsubscribes, unsubscribe)
+				fmt.Printf("Registered listener for %s.%s\n", storeName, key)
+			} else {
+				fmt.Printf("Store %s not found\n", storeName)
+			}
+		}
+
+		var content string
+		if result {
+			content = ifContent
+		} else {
+			content = elseContent
+		}
+
+		return fmt.Sprintf(`<div data-condition="%s">%s</div>`, conditionID, content)
+	})
+
+	// Manage @if without @else
+	ifRegexNoElse := regexp.MustCompile(`(@if:.+)([\S\s]+)(@endif)`)
+	template = ifRegexNoElse.ReplaceAllStringFunc(template, func(match string) string {
+		parts := ifRegexNoElse.FindStringSubmatch(match)
+		if len(parts) < 3 {
+			return match
+		}
+
+		conditionStr := strings.TrimSpace(parts[1])
+		ifContent := parts[2]
+
+		conditionID := fmt.Sprintf("cond-%x", sha1.Sum([]byte(conditionStr)))
+
+		result, dependencies := evaluateCondition(conditionStr, c)
+
+		c.conditionContents[conditionID] = ConditionContent{
+			conditionStr: conditionStr,
+			ifContent:    ifContent,
+			elseContent:  "",
+		}
+
+		for _, dep := range dependencies {
+			storeName, key := dep.storeName, dep.key
+			fmt.Printf("Registering listener for condition '%s' on store '%s', key '%s'\n", conditionStr, storeName, key)
+			store := GlobalStoreManager.GetStore(storeName)
+			if store != nil {
+				unsubscribe := store.OnChange(key, func(newValue interface{}) {
+					fmt.Printf("Listener triggered for store '%s', key '%s', new value: '%v'\n", storeName, key, newValue)
+					UpdateConditionBindings(c, conditionID, conditionStr)
+				})
+				c.unsubscribes = append(c.unsubscribes, unsubscribe)
+			} else {
+				fmt.Printf("Store '%s' not found when registering listener.\n", storeName)
+			}
+		}
+
+		var content string
+		if result {
+			content = ifContent
+		} else {
+			content = ""
+		}
+
+		return fmt.Sprintf(`<div data-condition="%s">%s</div>`, conditionID, content)
+	})
 
 	return template
 }
 
-func evaluateCondition(condition string, c *HTMLComponent) bool {
+func evaluateCondition(condition string, c *HTMLComponent) (bool, []ConditionDependency) {
+	fmt.Printf("Evaluating condition: '%s'\n", condition)
 	conditionParts := strings.Split(condition, "==")
 	if len(conditionParts) != 2 {
-		return false
+		fmt.Println("Condition format is invalid. Expected '=='.")
+		return false, nil
 	}
 
 	leftSide := strings.TrimSpace(conditionParts[0])
 	leftSide = strings.Replace(leftSide, "@if:", "", 1)
-	leftSide = strings.Replace(leftSide, "@elseif:", "", 1)
 	expectedValue := strings.ReplaceAll(conditionParts[1], `"`, "")
 	expectedValue = strings.TrimSpace(expectedValue)
+
+	fmt.Printf("Left side: '%s', Expected value: '%s'\n", leftSide, expectedValue)
+
+	dependencies := []ConditionDependency{}
 
 	if strings.HasPrefix(leftSide, "store:") {
 		storeParts := strings.Split(strings.TrimPrefix(leftSide, "store:"), ".")
 		if len(storeParts) == 2 {
 			storeName, key := storeParts[0], storeParts[1]
+			fmt.Printf("Dependency detected: Store '%s', Key '%s'\n", storeName, key)
 			store := GlobalStoreManager.GetStore(storeName)
 			if store != nil {
+				dependencies = append(dependencies, ConditionDependency{storeName, key})
 				actualValue := fmt.Sprintf("%v", store.Get(key))
-				fmt.Printf("Comparing %s with %s\n", actualValue, expectedValue)
-				return actualValue == expectedValue
+				fmt.Printf("Actual value from store: '%s'\n", actualValue)
+				return actualValue == expectedValue, dependencies
+			} else {
+				fmt.Printf("Store '%s' not found.\n", storeName)
 			}
+		} else {
+			fmt.Println("Store parts length is not 2.")
 		}
 	}
 
@@ -264,9 +358,85 @@ func evaluateCondition(condition string, c *HTMLComponent) bool {
 		propName := strings.TrimPrefix(leftSide, "prop:")
 		if value, exists := c.Props[propName]; exists {
 			actualValue := fmt.Sprintf("%v", value)
-			return actualValue == expectedValue
+			fmt.Printf("Actual value from props: '%s'\n", actualValue)
+			return actualValue == expectedValue, dependencies
 		}
 	}
 
-	return false
+	fmt.Println("No dependencies detected.")
+	return false, dependencies
+}
+
+func UpdateConditionBindings(c *HTMLComponent, conditionID, conditionStr string) {
+	document := js.Global().Get("document")
+	var element js.Value
+	if c.ID == "" {
+		element = document.Call("getElementById", "app")
+	} else {
+		element = document.Call("querySelector", fmt.Sprintf("[data-component-id='%s']", c.ID))
+	}
+	if element.IsNull() || element.IsUndefined() {
+		return
+	}
+
+	selector := fmt.Sprintf(`[data-condition="%s"]`, conditionID)
+	node := element.Call("querySelector", selector)
+	if node.IsNull() || node.IsUndefined() {
+		return
+	}
+
+	result, _ := evaluateCondition(conditionStr, c)
+
+	conditionContent := c.conditionContents[conditionID]
+	var newContent string
+	if result {
+		newContent = conditionContent.ifContent
+	} else {
+		newContent = conditionContent.elseContent
+	}
+
+	node.Set("innerHTML", newContent)
+
+	bindStoreInputs(node)
+}
+
+func UpdateConditionsForStoreVariable(c *HTMLComponent, storeName, key string) {
+	for conditionID, content := range c.conditionContents {
+		dependencies, _ := getConditionDependencies(content.conditionStr)
+		for _, dep := range dependencies {
+			if dep.storeName == storeName && dep.key == key {
+				UpdateConditionBindings(c, conditionID, content.conditionStr)
+				break
+			}
+		}
+	}
+}
+
+func UpdateStoreBindings(c *HTMLComponent, storeName, key string, newValue interface{}) {
+	document := js.Global().Get("document")
+	var element js.Value
+	if c.ID == "" {
+		element = document.Call("getElementById", "app")
+	} else {
+		element = document.Call("querySelector", fmt.Sprintf("[data-component-id='%s']", c.ID))
+	}
+	if element.IsNull() || element.IsUndefined() {
+		return
+	}
+
+	selector := fmt.Sprintf(`[data-store="%s.%s"]`, storeName, key)
+	nodes := element.Call("querySelectorAll", selector)
+	for i := 0; i < nodes.Length(); i++ {
+		node := nodes.Index(i)
+		node.Set("innerHTML", fmt.Sprintf("%v", newValue))
+	}
+
+	inputSelector := fmt.Sprintf(`input[value="@store:%s.%s:w"], select[value="@store:%s.%s:w"], textarea[value="@store:%s.%s:w"]`, storeName, key, storeName, key, storeName, key)
+	inputs := element.Call("querySelectorAll", inputSelector)
+	for i := 0; i < inputs.Length(); i++ {
+		input := inputs.Index(i)
+		input.Set("value", fmt.Sprintf("%v", newValue))
+	}
+
+	UpdateConditionsForStoreVariable(c, storeName, key)
 }
