c98a29e3f4c61e8cbbcd724de06f31d66e232d92
Gabriel Cozma <me@gabs.eu.org>
2024-09-21 11:11:22 +0300

    feat: introduce a new command style and more
    - Added the help popup
    - Added the open browser & close signals
    - Add local IP shown with --host

--- go.mod
+++ go.mod
@@ -3,3 +3,10 @@ module github.com/mirkobrombin/rfw
 go 1.22.3
 
 require golang.org/x/net v0.29.0
+
+require (
+	github.com/fatih/color v1.17.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	golang.org/x/sys v0.25.0 // indirect
+)
--- server/main.go
+++ server/main.go
@@ -1,27 +1,172 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
+	"net"
 	"net/http"
 	"os"
+	"os/exec"
+	"os/signal"
+	"strings"
+	"syscall"
+
+	"github.com/fatih/color"
 )
 
 const port = "8080"
 
+var (
+	green      = color.New(color.FgGreen).SprintFunc()
+	white      = color.New(color.FgWhite).SprintFunc()
+	bold       = color.New(color.FgWhite, color.Bold).SprintFunc()
+	faint      = color.New(color.FgWhite, color.Faint).SprintFunc()
+	faintGreen = color.New(color.FgGreen, color.Faint).SprintFunc()
+	boldGreen  = color.New(color.FgGreen, color.Bold).SprintFunc()
+	indent     = "  "
+)
+
 func main() {
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM)
+
 	fs := http.FileServer(http.Dir("."))
-
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		log.Println("Serving", r.URL.Path)
-
-		if _, err := os.Stat("." + r.URL.Path); os.IsNotExist(err) {
-			http.ServeFile(w, r, "./index.html")
-		} else {
-			fs.ServeHTTP(w, r)
-		}
+		log.Println(bold("Serving"), r.URL.Path)
+		handleFileRequest(w, r, fs)
 	})
 
-	log.Println("Server running on port", port)
-	http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
+	localIP, err := getLocalIP()
+	if err != nil {
+		log.Fatalf("Failed to get local IP address: %v", err)
+	}
+
+	clearScreen()
+	printStartupInfo(localIP)
+
+	go startServer()
+	go listenForShutdown(stop)
+
+	<-stop
+	fmt.Println("Server stopped.")
+}
+
+func handleFileRequest(w http.ResponseWriter, r *http.Request, fs http.Handler) {
+	if _, err := os.Stat("." + r.URL.Path); os.IsNotExist(err) {
+		http.ServeFile(w, r, "./index.html")
+	} else {
+		fs.ServeHTTP(w, r)
+	}
+}
+
+func clearScreen() {
+	fmt.Print("\033[H\033[2J")
+	fmt.Println()
+}
+
+func printStartupInfo(localIP string) {
+	fmt.Println(indent, boldGreen("rfw"), faint("v0.0.0"))
+	fmt.Println()
+	fmt.Println(indent, green("➜ "), bold("Local:"), green(fmt.Sprintf("http://localhost:%s/", port)))
+
+	if len(os.Args) > 1 && os.Args[1] == "--host" {
+		fmt.Println(indent, green("➜ "), faint(bold("Network:")), white(fmt.Sprintf("http://%s:%s/", localIP, port)))
+	} else {
+		fmt.Println(indent, green("➜ "), faint(bold("Network:")), white("--host"), faint("to expose"))
+	}
+
+	fmt.Println(indent, faintGreen("➜ "), faint("Press"), bold("h + enter"), faint("to show help"))
+	fmt.Println()
+}
+
+func startServer() {
+	if err := http.ListenAndServe(fmt.Sprintf(":%s", port), nil); err != nil {
+		log.Fatalf("Server failed: %v", err)
+	}
+}
+
+func listenForShutdown(stop chan os.Signal) {
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		input, _ := reader.ReadString('\n')
+		input = strings.TrimSpace(input)
+
+		if strings.ToLower(input) == "h" {
+			fmt.Println()
+			fmt.Println(indent, green("➜ "), bold("Help (Shortcuts)"))
+			fmt.Println(indent, indent, faint("Press"), bold("c + enter"), faint("to stop the server"))
+			fmt.Println(indent, indent, faint("Press"), bold("o + enter"), faint("to open the browser"))
+			fmt.Println(indent, indent, faint("Press"), bold("u + enter"), faint("to show the startup info and clear logs"))
+			fmt.Println(indent, indent, faint("Press"), bold("h + enter"), faint("to show this help"))
+			fmt.Println()
+		}
+
+		if strings.ToLower(input) == "u" {
+			fmt.Println("Showing the startup info and clear logs...")
+			clearScreen()
+			localIP, err := getLocalIP()
+			if err != nil {
+				log.Fatalf("Failed to get local IP address: %v", err)
+			}
+			printStartupInfo(localIP)
+		}
+
+		if strings.ToLower(input) == "r" {
+			fmt.Println("Reloading the server...")
+			clearScreen()
+			go startServer()
+		}
+
+		if strings.ToLower(input) == "c" || strings.ToLower(input) == "q" {
+			fmt.Println("Closing the server...")
+			close(stop)
+			break
+		}
+
+		if strings.ToLower(input) == "o" {
+			fmt.Println("Opening the browser...")
+			url := fmt.Sprintf("http://localhost:%s/", port)
+			openBrowser(url)
+		}
+	}
+}
+
+func openBrowser(url string) {
+	var cmd *exec.Cmd
+	switch {
+	case isWindows():
+		cmd = exec.Command("cmd", "/c", "start", url)
+	case isMac():
+		cmd = exec.Command("open", url)
+	default:
+		cmd = exec.Command("xdg-open", url)
+	}
+
+	if err := cmd.Start(); err != nil {
+		fmt.Printf("Failed to open browser: %v\n", err)
+	}
+}
+
+func isWindows() bool {
+	return strings.Contains(strings.ToLower(os.Getenv("OS")), "windows")
+}
+
+func isMac() bool {
+	return strings.Contains(strings.ToLower(os.Getenv("HOME")), "/users/")
+}
+
+func getLocalIP() (string, error) {
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		return "", err
+	}
+
+	for _, addr := range addrs {
+		if ipNet, ok := addr.(*net.IPNet); ok && !ipNet.IP.IsLoopback() && ipNet.IP.To4() != nil {
+			return ipNet.IP.String(), nil
+		}
+	}
+
+	return "", fmt.Errorf("no local IP address found")
 }
