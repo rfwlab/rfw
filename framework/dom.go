f0ac4d220d4d0d1a33d4d7195318cff5017323e5
Mirko Brombin <brombin94@gmail.com>
2025-07-09 15:48:30 +0200

    feat: patch dom instead of always replacing

--- framework/dom.go
+++ framework/dom.go
@@ -19,7 +19,8 @@ func UpdateDOM(componentID string, html string) {
 	if element.IsNull() || element.IsUndefined() {
 		return
 	}
-	element.Set("innerHTML", html)
+
+	patchInnerHTML(element, html)
 
 	bindStoreInputs(element)
 }
@@ -49,6 +50,76 @@ func bindStoreInputs(element js.Value) {
 					return nil
 				}))
 			}
+		}
+	}
+}
+
+func patchInnerHTML(element js.Value, html string) {
+	document := js.Global().Get("document")
+	template := document.Call("createElement", "template")
+	template.Set("innerHTML", html)
+	newContent := template.Get("content")
+	patchChildren(element, newContent)
+}
+
+func patchChildren(oldParent, newParent js.Value) {
+	oldChildren := oldParent.Get("childNodes")
+	newChildren := newParent.Get("childNodes")
+	oldLen := oldChildren.Length()
+	newLen := newChildren.Length()
+
+	minLen := oldLen
+	if newLen < minLen {
+		minLen = newLen
+	}
+
+	for i := 0; i < minLen; i++ {
+		patchNode(oldChildren.Index(i), newChildren.Index(i))
+	}
+
+	for i := minLen; i < newLen; i++ {
+		clone := newChildren.Index(i).Call("cloneNode", true)
+		oldParent.Call("appendChild", clone)
+	}
+
+	for i := oldLen - 1; i >= minLen; i-- {
+		oldChildren.Index(i).Call("remove")
+	}
+}
+
+func patchNode(oldNode, newNode js.Value) {
+	nodeType := newNode.Get("nodeType").Int()
+	if nodeType == 3 { // Text node
+		if oldNode.Get("nodeValue").String() != newNode.Get("nodeValue").String() {
+			oldNode.Set("nodeValue", newNode.Get("nodeValue"))
+		}
+		return
+	}
+
+	if oldNode.Get("nodeName").String() != newNode.Get("nodeName").String() {
+		oldNode.Call("replaceWith", newNode.Call("cloneNode", true))
+		return
+	}
+
+	patchAttributes(oldNode, newNode)
+	patchChildren(oldNode, newNode)
+}
+
+func patchAttributes(oldNode, newNode js.Value) {
+	oldAttrs := oldNode.Call("getAttributeNames")
+	for i := 0; i < oldAttrs.Length(); i++ {
+		name := oldAttrs.Index(i).String()
+		if !newNode.Call("hasAttribute", name).Bool() {
+			oldNode.Call("removeAttribute", name)
+		}
+	}
+
+	newAttrs := newNode.Call("getAttributeNames")
+	for i := 0; i < newAttrs.Length(); i++ {
+		name := newAttrs.Index(i).String()
+		val := newNode.Call("getAttribute", name)
+		if oldNode.Call("getAttribute", name).String() != val.String() {
+			oldNode.Call("setAttribute", name, val)
 		}
 	}
 }
