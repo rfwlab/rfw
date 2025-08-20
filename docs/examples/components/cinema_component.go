7adc1be56b9c2915d27c4dfb2d99ab38dac110f3
Mirko Brombin <brombin94@gmail.com>
2025-08-20 22:34:25 +0200

    feat: cinema api

--- docs/examples/components/cinema_component.go
+++ docs/examples/components/cinema_component.go
@@ -13,16 +13,76 @@ import (
 //go:embed templates/cinema_component.rtml
 var cinemaComponentTpl []byte
 
+var cinema *anim.CinemaBuilder
+
 func NewCinemaComponent() *core.HTMLComponent {
 	c := core.NewComponent("CinemaComponent", cinemaComponentTpl, nil)
-	dom.RegisterHandlerFunc("runCinema", func() {
-		anim.NewCinemaBuilder("#cinemaRoot").
-			AddScene("#sceneBox", map[string]any{"duration": 1000}).
-			AddKeyFrame(map[string]any{"transform": "translateX(0px)"}, 0).
-			AddKeyFrame(map[string]any{"transform": "translateX(100px)"}, 1).
-			AddVideo("#sceneVideo").
-			PlayVideo().
-			Play()
+
+	dom.RegisterHandlerFunc("playCinema", func() {
+		if cinema == nil {
+			cinema = anim.NewCinemaBuilder("#cinemaRoot").
+				AddScene("#sceneBox", map[string]any{"duration": 1000}).
+				AddKeyFrame(map[string]any{"transform": "translateX(0px)"}, 0).
+				AddKeyFrame(map[string]any{"transform": "translateX(100px)"}, 1).
+				AddVideo("#sceneVideo").
+				BindProgress("#progressBar")
+		}
+		cinema.PlayVideo().Play()
 	})
+
+	dom.RegisterHandlerFunc("pauseCinema", func() {
+		if cinema != nil {
+			cinema.PauseVideo()
+		}
+	})
+
+	dom.RegisterHandlerFunc("stopCinema", func() {
+		if cinema != nil {
+			cinema.StopVideo()
+		}
+	})
+
+	dom.RegisterHandlerFunc("seekCinema", func() {
+		if cinema != nil {
+			cinema.SeekVideo(5)
+		}
+	})
+
+	dom.RegisterHandlerFunc("speedCinema", func() {
+		if cinema != nil {
+			cinema.SetPlaybackRate(1.5)
+		}
+	})
+
+	dom.RegisterHandlerFunc("volumeCinema", func() {
+		if cinema != nil {
+			cinema.SetVolume(0.5)
+		}
+	})
+
+	dom.RegisterHandlerFunc("muteCinema", func() {
+		if cinema != nil {
+			cinema.MuteVideo()
+		}
+	})
+
+	dom.RegisterHandlerFunc("unmuteCinema", func() {
+		if cinema != nil {
+			cinema.UnmuteVideo()
+		}
+	})
+
+	dom.RegisterHandlerFunc("subtitleCinema", func() {
+		if cinema != nil {
+			cinema.AddSubtitle("subtitles", "English", "en", "https://www.w3schools.com/html/mov_bbb_subtitles_en.vtt")
+		}
+	})
+
+	dom.RegisterHandlerFunc("audioCinema", func() {
+		if cinema != nil {
+			cinema.AddAudio("https://www.w3schools.com/html/horse.mp3")
+		}
+	})
+
 	return c
 }
