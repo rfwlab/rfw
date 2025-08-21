8721120d24a1b04cdca6d14cefd28314cf93a066
Mirko Brombin <brombin94@gmail.com>
2025-08-21 19:35:47 +0200

    feat: (WIP) SSR, hydratation
    also restore SPA wasm build

--- cmd/rfw/build/build.go
+++ cmd/rfw/build/build.go
@@ -1,62 +1,67 @@
 package build
 
 import (
-        "encoding/json"
-        "fmt"
-        "io"
-        "os"
-        "os/exec"
-        "path/filepath"
-        "strings"
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
 
-        "github.com/rfwlab/rfw/cmd/rfw/plugins"
-        _ "github.com/rfwlab/rfw/cmd/rfw/plugins/assets"
-        _ "github.com/rfwlab/rfw/cmd/rfw/plugins/tailwind"
-        _ "github.com/rfwlab/rfw/cmd/rfw/plugins/test"
+	"github.com/rfwlab/rfw/cmd/rfw/plugins"
+	_ "github.com/rfwlab/rfw/cmd/rfw/plugins/assets"
+	_ "github.com/rfwlab/rfw/cmd/rfw/plugins/tailwind"
+	_ "github.com/rfwlab/rfw/cmd/rfw/plugins/test"
 )
 
 func Build(_ map[string]any) error {
-        var manifest struct {
-                Build struct {
-                        Type  string `json:"type"`
-                        OutDir string `json:"outDir"`
-                } `json:"build"`
-                Plugins map[string]json.RawMessage `json:"plugins"`
-        }
-        if data, err := os.ReadFile("rfw.json"); err == nil {
-                _ = json.Unmarshal(data, &manifest)
-        }
-        outDir := manifest.Build.OutDir
-        if outDir == "" {
-                outDir = "dist"
-        }
-        clientDir := filepath.Join(outDir, "client")
-        if err := os.MkdirAll(clientDir, 0o755); err != nil {
-                return err
-        }
+	var manifest struct {
+		Build struct {
+			Type   string `json:"type"`
+			OutDir string `json:"outDir"`
+		} `json:"build"`
+		Plugins map[string]json.RawMessage `json:"plugins"`
+	}
+	if data, err := os.ReadFile("rfw.json"); err == nil {
+		_ = json.Unmarshal(data, &manifest)
+	}
 
-        goroot, err := exec.Command("go", "env", "GOROOT").Output()
-        if err != nil {
-                return fmt.Errorf("failed to get GOROOT: %w", err)
-        }
-        wasmExec := filepath.Join(strings.TrimSpace(string(goroot)), "lib", "wasm", "wasm_exec.js")
-        if err := copyFile(wasmExec, filepath.Join(clientDir, "wasm_exec.js")); err != nil {
-                return fmt.Errorf("failed to copy wasm_exec.js: %w", err)
-        }
+	outDir := "."
+	clientDir := filepath.Join(outDir, "")
+	if manifest.Build.Type == "ssr" {
+		outDir = manifest.Build.OutDir
+		if outDir == "" {
+			outDir = "dist"
+		}
+		clientDir = filepath.Join(outDir, "client")
+		if err := os.MkdirAll(clientDir, 0o755); err != nil {
+			return err
+		}
+	}
 
-        outputWasm := filepath.Join(clientDir, "app.wasm")
-        cmd := exec.Command("go", "build", "-o", outputWasm, "main.go")
-        cmd.Env = append(os.Environ(), "GOARCH=wasm", "GOOS=js")
-        output, err := cmd.CombinedOutput()
-        if err != nil {
-                return fmt.Errorf("failed to build project: %s: %w", output, err)
-        }
+	goroot, err := exec.Command("go", "env", "GOROOT").Output()
+	if err != nil {
+		return fmt.Errorf("failed to get GOROOT: %w", err)
+	}
+	wasmExec := filepath.Join(strings.TrimSpace(string(goroot)), "lib", "wasm", "wasm_exec.js")
+	if err := copyFile(wasmExec, filepath.Join(clientDir, "wasm_exec.js")); err != nil {
+		return fmt.Errorf("failed to copy wasm_exec.js: %w", err)
+	}
 
-        if err := plugins.Configure(manifest.Plugins); err != nil {
-                return fmt.Errorf("failed to run plugins: %w", err)
-        }
+	outputWasm := filepath.Join(clientDir, "app.wasm")
+	cmd := exec.Command("go", "build", "-o", outputWasm, "main.go")
+	cmd.Env = append(os.Environ(), "GOARCH=wasm", "GOOS=js")
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		return fmt.Errorf("failed to build project: %s: %w", output, err)
+	}
 
-        return nil
+	if err := plugins.Configure(manifest.Plugins); err != nil {
+		return fmt.Errorf("failed to run plugins: %w", err)
+	}
+
+	return nil
 }
 
 func copyFile(src, dst string) error {
