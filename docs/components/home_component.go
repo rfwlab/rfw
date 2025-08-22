91abe140fa0c1facd71fb5069b157d1e2aedc28d
Mirko Brombin <brombin94@gmail.com>
2025-08-22 13:46:20 +0200

    chore: use rfw APIs

--- docs/components/home_component.go
+++ docs/components/home_component.go
@@ -6,9 +6,6 @@ import (
 	_ "embed"
 
 	"github.com/rfwlab/rfw/v1/core"
-	events "github.com/rfwlab/rfw/v1/events"
-	js "github.com/rfwlab/rfw/v1/js"
-	"github.com/rfwlab/rfw/v1/router"
 )
 
 //go:embed templates/home_component.rtml
@@ -17,22 +14,5 @@ var homeTpl []byte
 func NewHomeComponent() *core.HTMLComponent {
 	c := core.NewComponent("HomeComponent", homeTpl, nil)
 	c.AddHostComponent("HomeHost")
-	c.SetOnMount(func(cmp *core.HTMLComponent) {
-		doc := js.Document()
-		add := func(sel, path string) {
-			if el := doc.Call("querySelector", sel); el.Truthy() {
-				ch := events.Listen("click", el)
-				go func(p string) {
-					for evt := range ch {
-						evt.Call("preventDefault")
-						router.Navigate(p)
-					}
-				}(path)
-			}
-		}
-		add("a[href='/']", "/")
-		add("a[href='/docs/index']", "/docs/index")
-		add("a[href='/docs/getting-started']", "/docs/getting-started")
-	})
 	return c
 }
