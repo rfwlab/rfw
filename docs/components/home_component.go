eea17579bf85c796c10a80246faf129277684b1d
Mirko Brombin <brombin94@gmail.com>
2025-08-25 13:55:25 +0200

    cleanup: upload assets

--- docs/components/home_component.go
+++ docs/components/home_component.go
@@ -7,8 +7,9 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/rfwlab/rfw/v1/core"
 	"syscall/js"
+
+	"github.com/rfwlab/rfw/v1/core"
 )
 
 //go:embed templates/home_component.rtml
@@ -18,11 +19,10 @@ func NewHomeComponent() *core.HTMLComponent {
 	c := core.NewComponent("HomeComponent", homeTpl, nil)
 
 	snippets := []map[string]string{
-		{"title": "GO component 1", "image": "https://cdn.gxbs.dev/HeRO2/ZipiBEQe56.png/raw"},
-		{"title": "Go component 2", "image": "https://cdn.gxbs.dev/HeRO2/gUsoDUxU99.png/raw"},
-		{"title": "RTML", "image": "https://cdn.gxbs.dev/HeRO2/qIrawiBa17.png/raw"},
-		{"title": "Browser Preview", "image": "https://cdn.gxbs.dev/HeRO2/ZimIsErA38.png/raw"},
-
+		{"title": "GO component 1", "image": "/slide-1.png"},
+		{"title": "Go component 2", "image": "/slide-2.png"},
+		{"title": "RTML", "image": "/slide-3.png"},
+		{"title": "Browser Preview", "image": "/slide-4.png"},
 	}
 
 	// Track viewed snippets
@@ -35,7 +35,7 @@ func NewHomeComponent() *core.HTMLComponent {
 		}
 		snippet := snippets[index]
 		progressBars := ""
-		for i := 0; i < len(snippets); i++ {
+		for i := range len(snippets) {
 			progressBars += fmt.Sprintf(
 				`<div class="h-1 rounded-full flex-1 mx-1 bg-white/20">
 					<div class="h-1 rounded-full %s %s"></div>
@@ -56,7 +56,7 @@ func NewHomeComponent() *core.HTMLComponent {
 		}
 		html := fmt.Sprintf(`
 				<div class="relative w-full p-3 pt-11 rounded-2xl shadow-2xl text-white overflow-hidden"
-						style="background-image: url('https://cdn.gxbs.dev/HeRO2/haqIFIJe53.png/raw'); background-size: cover; background-position: center; min-height: 400px; min-width: 400px;">
+						style="background-image: url('/slide-bg.png'); background-size: cover; background-position: center; min-height: 400px; min-width: 400px;">
 
 					<!-- Image block -->
 					<img src="%s" alt="%s" class="w-full h-auto rounded-md shadow-lg opacity-0 transition-opacity duration-500" id="story-image" style="min-height: 200px; min-width: 300px;">
@@ -71,7 +71,7 @@ func NewHomeComponent() *core.HTMLComponent {
 		container := js.Global().Get("document").Call("getElementById", "examples")
 		container.Set("innerHTML", html)
 
-		js.Global().Call("setTimeout", js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+		js.Global().Call("setTimeout", js.FuncOf(func(this js.Value, args []js.Value) any {
 			img := js.Global().Get("document").Call("getElementById", "story-image")
 			if !img.IsNull() {
 				img.Get("classList").Call("remove", "opacity-0")
