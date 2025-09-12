9f3a5ac1acfd09717cb41c169428401eff05456f
Mirko Brombin <brombin94@gmail.com>
2025-09-12 17:21:05 +0200

    feat: shims APIs

---
+++ v1/js/shim/highlightjs/highlight.go
@@ -0,0 +1,19 @@
+//go:build js && wasm
+
+package highlightjs
+
+import (
+	js "github.com/rfwlab/rfw/v1/js"
+)
+
+// RegisterLanguage wires a Highlight.js language definition from Go.
+// The def callback receives the hljs object and must return the language config.
+func RegisterLanguage(name string, def func(hljs js.Value) js.Value) {
+	hljsObj := js.Global().Get("hljs")
+	hljsObj.Call("registerLanguage", name, js.FuncOf(func(this js.Value, args []js.Value) any {
+		if len(args) > 0 {
+			return def(args[0])
+		}
+		return nil
+	}))
+}
