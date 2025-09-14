31cff5143954600289d2c8c4e6c34ea356316202
Mirko Brombin <brombin94@gmail.com>
2025-09-14 19:13:25 +0200

    feat: Extend Composition APIs

--- docs/examples/plugins/toast_component.go
+++ docs/examples/plugins/toast_component.go
@@ -5,6 +5,7 @@ package plugins
 import (
 	_ "embed"
 
+	"github.com/rfwlab/rfw/v1/composition"
 	core "github.com/rfwlab/rfw/v1/core"
 	dom "github.com/rfwlab/rfw/v1/dom"
 	"github.com/rfwlab/rfw/v1/plugins/toast"
@@ -13,13 +14,13 @@ import (
 //go:embed templates/toast_component.rtml
 var toastTpl []byte
 
-type ToastComponent struct{ *core.HTMLComponent }
+type ToastComponent struct{ *composition.Component }
 
 func NewToastComponent() *ToastComponent {
-	c := &ToastComponent{}
-	c.HTMLComponent = core.NewComponent("ToastComponent", toastTpl, nil)
-	dom.RegisterHandlerFunc("basic", c.basic)
-	dom.RegisterHandlerFunc("custom", c.custom)
+	cmp := composition.Wrap(core.NewComponent("ToastComponent", toastTpl, nil))
+	c := &ToastComponent{Component: cmp}
+	cmp.On("basic", c.basic)
+	cmp.On("custom", c.custom)
 	return c
 }
 
@@ -33,23 +34,15 @@ func (c *ToastComponent) basic() {
 
 func (c *ToastComponent) custom() {
 	tpl := func(msg string, acts []toast.Action, close func()) dom.Element {
-		doc := dom.Doc()
-		el := doc.CreateElement("div")
-		el.AddClass("bg-green-700")
-		el.AddClass("text-white")
-		el.AddClass("px-4")
-		el.AddClass("py-2")
-		el.AddClass("mb-2")
-		el.AddClass("rounded")
-		span := doc.CreateElement("span")
-		span.SetText(msg)
-		el.Call("appendChild", span.Value)
-		btn := doc.CreateElement("button")
-		btn.SetText("Got it")
-		btn.AddClass("ml-4")
-		btn.OnClick(func(dom.Event) { close() })
-		el.Call("appendChild", btn.Value)
-		return el
+		div := composition.Div().Classes(
+			"bg-green-700", "text-white", "px-4", "py-2", "mb-2", "rounded",
+		)
+		span := composition.Span().Text(msg)
+		div.Element().AppendChild(span.Element())
+		btn := composition.Button().Class("ml-4").Text("Got it")
+		btn.Element().OnClick(func(dom.Event) { close() })
+		div.Element().AppendChild(btn.Element())
+		return div.Element()
 	}
 	toast.PushOptions("Custom!", toast.Options{Template: tpl})
 }
