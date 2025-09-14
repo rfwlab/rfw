29f018b3d6977226562c7465d6d1d653de6398c4
Mirko Brombin <brombin94@gmail.com>
2025-09-14 18:46:24 +0200

    feat: move ExampleFrame to composition APIs

--- docs/components/example_frame.go
+++ docs/components/example_frame.go
@@ -4,71 +4,60 @@ package components
 
 import (
 	_ "embed"
+	"errors"
 	"fmt"
+	"time"
 
+	"github.com/rfwlab/rfw/v1/composition"
 	core "github.com/rfwlab/rfw/v1/core"
-	events "github.com/rfwlab/rfw/v1/events"
-	js "github.com/rfwlab/rfw/v1/js"
+	"github.com/rfwlab/rfw/v1/dom"
+	"github.com/rfwlab/rfw/v1/http"
+	highlight "github.com/rfwlab/rfw/v1/plugins/highlight"
 )
 
 //go:embed templates/example_frame.rtml
 var exampleFrameTpl []byte
 
-type exampleFrame struct {
-	*core.HTMLComponent
-}
-
 func NewExampleFrame(props map[string]any) *core.HTMLComponent {
-	ef := &exampleFrame{}
-	ef.HTMLComponent = core.NewComponent("ExampleFrame", exampleFrameTpl, props).WithLifecycle(ef.mount, nil)
-	return ef.HTMLComponent
-}
+	cmp := composition.Wrap(core.NewComponent("ExampleFrame", exampleFrameTpl, props))
 
-func init() {
-	core.MustRegisterComponent("ExampleFrame", func() core.Component { return NewExampleFrame(nil) })
-}
-
-func (e *exampleFrame) mount(hc *core.HTMLComponent) {
-	wrapper := hc.GetRef("wrapper")
-	if !wrapper.Truthy() {
-		return
-	}
-	frame := hc.GetRef("frame")
-	if uri, ok := hc.Props["uri"].(string); ok {
-		uri = fmt.Sprintf(`%s?%s`, uri, hc.GetID())
-		frame.SetAttr("src", uri)
-	}
-	codeTab := hc.GetRef("codeTab")
-	previewTab := hc.GetRef("previewTab")
-	codeDiv := hc.GetRef("codeDiv")
-	codeEl := hc.GetRef("codeEl")
-	filePathEl := hc.GetRef("filePath")
-	previewDiv := hc.GetRef("previewDiv")
-	if codeURL, ok := hc.Props["code"].(string); ok && codeURL != "" {
-		filePathEl.SetText(codeURL)
-
-		js.Fetch(codeURL).Call("then", js.FuncOf(func(this js.Value, args []js.Value) any {
-			resp := args[0]
-			return resp.Call("text").Call("then", js.FuncOf(func(this js.Value, args []js.Value) any {
-				code := args[0].String()
-				codeEl.SetText(code)
-				if h := js.Get("rfwHighlight"); h.Truthy() {
-					res := h.Invoke(code, "go")
-					if res.Truthy() && res.String() != "" {
-						codeEl.SetHTML(res.String())
-					} else if hljs := js.Get("hljs"); hljs.Truthy() {
-						hljs.Call("highlightElement", codeEl.Value)
+	cmp.SetOnMount(func(*core.HTMLComponent) {
+		wrapper := cmp.GetRef("wrapper")
+		if !wrapper.Truthy() {
+			return
+		}
+		frame := cmp.GetRef("frame")
+		if uri, ok := cmp.Props["uri"].(string); ok {
+			uri = fmt.Sprintf(`%s?%s`, uri, cmp.GetID())
+			frame.SetAttr("src", uri)
+		}
+		codeTab := cmp.GetRef("codeTab")
+		previewTab := cmp.GetRef("previewTab")
+		codeDiv := cmp.GetRef("codeDiv")
+		codeEl := cmp.GetRef("codeEl")
+		filePathEl := cmp.GetRef("filePath")
+		previewDiv := cmp.GetRef("previewDiv")
+		if codeURL, ok := cmp.Props["code"].(string); ok && codeURL != "" {
+			filePathEl.SetText(codeURL)
+			go func() {
+				for {
+					code, err := http.FetchText(codeURL)
+					if err != nil {
+						if errors.Is(err, http.ErrPending) {
+							time.Sleep(50 * time.Millisecond)
+							continue
+						}
+						filePathEl.SetText(err.Error())
+						return
 					}
-				} else if hljs := js.Get("hljs"); hljs.Truthy() {
-					hljs.Call("highlightElement", codeEl.Value)
+					codeEl.SetText(code)
+					highlight.HighlightAll()
+					return
 				}
-				return nil
-			}))
-		}))
-	}
-	codeCh := events.Listen("click", codeTab.Value)
-	go func() {
-		for range codeCh {
+			}()
+		}
+
+		codeTab.OnClick(func(dom.Event) {
 			codeDiv.RemoveClass("hidden")
 			previewDiv.AddClass("hidden")
 			codeTab.AddClass("border-b-2")
@@ -77,11 +66,9 @@ func (e *exampleFrame) mount(hc *core.HTMLComponent) {
 			previewTab.RemoveClass("border-b-2")
 			previewTab.RemoveClass("border-red-500")
 			previewTab.RemoveClass("text-red-500")
-		}
-	}()
-	previewCh := events.Listen("click", previewTab.Value)
-	go func() {
-		for range previewCh {
+		})
+
+		previewTab.OnClick(func(dom.Event) {
 			previewDiv.RemoveClass("hidden")
 			codeDiv.AddClass("hidden")
 			previewTab.AddClass("border-b-2")
@@ -90,6 +77,12 @@ func (e *exampleFrame) mount(hc *core.HTMLComponent) {
 			codeTab.RemoveClass("border-b-2")
 			codeTab.RemoveClass("border-red-500")
 			codeTab.RemoveClass("text-red-500")
-		}
-	}()
+		})
+	})
+
+	return cmp.Unwrap()
+}
+
+func init() {
+	core.MustRegisterComponent("ExampleFrame", func() core.Component { return NewExampleFrame(nil) })
 }
