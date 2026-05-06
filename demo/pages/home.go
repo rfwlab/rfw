70a7507de675ce759abf7fa691281acec775cb06
Mirko Brombin <brombin94@gmail.com>
2026-05-06 08:41:38 +0200

    fix: remove duplicate declarations in home.go

--- demo/pages/home.go
+++ demo/pages/home.go
@@ -24,15 +24,3 @@ func (h *HomePage) OnMount() {
 func NewHomePage() *t.View {
 	return composition.New(&HomePage{})
 }
-
-func (h *HomePage) Inc()  { h.Count.Set(h.Count.Get() + 1) }
-func (h *HomePage) Dec()  { h.Count.Set(h.Count.Get() - 1) }
-
-func (h *HomePage) OnMount() {
-	(&h.Count).Set(2)
-	(&h.Factor).Set(2)
-}
-
-func NewHomePage() *t.View {
-	return composition.New(&HomePage{})
-}
