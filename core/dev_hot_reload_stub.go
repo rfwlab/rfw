1497685d46a06af5ab7c079b12e354823a7dea37
mirkobrombin <brombin94@gmail.com>
2026-07-12 08:52:02 +0200

    style: gofmt stub files

--- core/dev_hot_reload_stub.go
+++ core/dev_hot_reload_stub.go
@@ -8,10 +8,10 @@ type HTMLComponent struct {
 }
 
 func (c *HTMLComponent) Render() string          { return "" }
-func (c *HTMLComponent) Mount()                   {}
-func (c *HTMLComponent) Unmount()                 {}
-func (c *HTMLComponent) OnMount()                 {}
-func (c *HTMLComponent) OnUnmount()               {}
-func (c *HTMLComponent) GetName() string          { return c.Name }
-func (c *HTMLComponent) GetID() string            { return c.ID }
-func (c *HTMLComponent) SetSlots(map[string]any)  {}
+func (c *HTMLComponent) Mount()                  {}
+func (c *HTMLComponent) Unmount()                {}
+func (c *HTMLComponent) OnMount()                {}
+func (c *HTMLComponent) OnUnmount()              {}
+func (c *HTMLComponent) GetName() string         { return c.Name }
+func (c *HTMLComponent) GetID() string           { return c.ID }
+func (c *HTMLComponent) SetSlots(map[string]any) {}
--- core/dev_noop_wasm.go
+++ core/dev_noop_wasm.go
@@ -3,5 +3,5 @@
 package core
 
 func devOverrideTemplate(c *HTMLComponent, template string) string { return template }
-func devRegisterComponent(*HTMLComponent) {}
-func devUnregisterComponent(*HTMLComponent) {}
+func devRegisterComponent(*HTMLComponent)                          {}
+func devUnregisterComponent(*HTMLComponent)                        {}
